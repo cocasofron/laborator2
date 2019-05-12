2f69cbe9a9a5f51d72f31692ff432a4fbe8961b5
corinasofron1 <corinasofron1@gmail.com>
2019-05-12 10:58:33 +0300

    First commit, includes model creation, migration and controller

---
+++ WebApplication3/Migrations/20190512075252_InitialCreation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication3.Migrations
+{
+    public partial class InitialCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Duration = table.Column<int>(nullable: false),
+                    Genre = table.Column<int>(nullable: false),
+                    ReleaseYear = table.Column<int>(nullable: false),
+                    DateAdded = table.Column<DateTime>(nullable: false),
+                    Director = table.Column<string>(nullable: true),
+                    Rating = table.Column<int>(nullable: false),
+                    Watched = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
---
+++ WebApplication3/Models/MoviesDbSeeder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class MoviesDbSeeder
+    {
+        public static void Initialize(MoviesDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any movies.
+            if (context.Movies.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Movies.AddRange(
+                new Movie
+                {
+                    Title = "SomeMovie",
+                    Description = "Description of the movie",
+                    Duration = 120,
+                    Genre = Genre.Action,
+                    ReleaseYear=2019,
+                    Director="Corina",
+                    Rating=7,
+                    Watched=true,
+                  
+
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
